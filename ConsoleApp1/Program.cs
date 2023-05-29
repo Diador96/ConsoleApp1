9f2a124601ff3c05f47dc97b10af5ea1526b0e3b
Кузнецов Константин <kuznecovksf@gmail.com>
2023-05-29 13:10:15 +0300

    заполнил анкету

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,14 @@ class MainClass
 {
     static void Main(string[] args)
     {
-        const string MyName = "Konstantyn";
-        Console.WriteLine(MyName);
-        Console.WriteLine("\nПривет, мир");
-        Console.WriteLine("\tМне 27 лет");
-        Console.WriteLine("My name is Konstantyn");
-        Console.WriteLine("\u0023");
+        string MyName = "Konstantyn";
+        byte MyAge = 27;
+        bool HaveIApet = true;
+        double MyShoeSize = 41.5;
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My hsoe size is " + MyShoeSize);
         Console.ReadKey();
     }
 }
