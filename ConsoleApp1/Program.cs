969abed37152904c6fbdbfbcaf4e8432ab732df5
Кузнецов Константин <kuznecovksf@gmail.com>
2023-05-29 12:59:54 +0300

    выполнил задания

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,15 @@
-﻿static void Main(string[] args)
+﻿using System;
+
+class MainClass
 {
-    string MyName;
-    MyName = "Konstantyn";
-    Console.WriteLine(MyName);
-    Console.ReadKey();
+    static void Main(string[] args)
+    {
+        const string MyName = "Konstantyn";
+        Console.WriteLine(MyName);
+        Console.WriteLine("\nПривет, мир");
+        Console.WriteLine("\tМне 27 лет");
+        Console.WriteLine("My name is Konstantyn");
+        Console.WriteLine("\u0023");
+        Console.ReadKey();
+    }
 }
